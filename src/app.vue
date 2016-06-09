74ba2ffeb4032ae37f63ed95b66526948e797e44
Pascal Martineau <pascal.animateur@gmail.com>
2016-06-09 08:45:56 -0400

    Configure sails-hook-vuewebpack with initial Vue.js frontend

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello Sails.js + Vue.js'
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  font-family: Helvetica, sans-serif;
+}
+</style>
